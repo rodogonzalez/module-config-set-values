3bbb815da8649e8f78cdcfc8f5f520986c679e8d
Rodolfo Gonzalez <42151698+rodogonzalez@users.noreply.github.com>
2025-03-07 11:36:02 -0600

    Update ExportController.php

--- src/Controller/ExportController.php
+++ src/Controller/ExportController.php
@@ -3,24 +3,18 @@
 namespace Drupal\your_module\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-
 use Symfony\Component\HttpFoundation\Response;
-
-/**
- * Defines HelloController class.
- */
 
 class ExportController extends ControllerBase
 {
   public function update_config()
   {
-    // Retrieve the configuration object.
-    $config = \Drupal::config('your_config_name');
 
-    // Update the value of a specific variable in the config.
-    $config->set('your_variable_name', 'new_value')->save();
+    $config = \Drupal::configFactory();
+    $editable_config = $config->getEditable('system.site');
+    $editable_config->set('name', 'Site Renamed :)')->save();
 
-    // Output a message confirming the update.    
+    // Output a message confirming the update.
     $response = new Response("The configuration variable has been updated successfully.\n");
     $response->headers->set('Content-Type', 'text/html');
     return $response;
